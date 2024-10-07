ac21511912abf246864adc4b81402e1554d256c4
Randal Morrow <58852805+rmorrow23@users.noreply.github.com>
2024-10-07 02:56:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// script.js
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
+}
+
+// Function to toggle project details visibility and button text
+function toggleDetails(projectId, buttonId) {
+    const projectCard = document.getElementById(projectId);
+    const expandButton = document.getElementById(buttonId);
+    projectCard.classList.toggle('expanded');
+
+    // Toggle button text based on the current state
+    if (projectCard.classList.contains('expanded')) {
+        expandButton.textContent = "See Less";
+    } else {
+        expandButton.textContent = "Click to Expand";
+    }
 }
