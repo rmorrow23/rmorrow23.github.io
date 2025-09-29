732e8e238da327670fd83d138f6d2e90ade50eee
Randal Morrow <58852805+rmorrow23@users.noreply.github.com>
2025-09-29 02:47:35 -0400

    Create Budget Tracker

---
+++ VaultView/Budget/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'finance-tracker-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html', // Add this if you rename your main HTML file
+  '/manifest.webmanifest'
+];
+
+// On install, cache the app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Caching App Shell');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// On activate, clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Clearing old cache');
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// On fetch, serve from cache first, then network
+self.addEventListener('fetch', (event) => {
+  // We only want to cache GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
