045647eb719b2f2d7c258ad532078a1bb9de9a8e
Randal Morrow <randalmorrow24@gmail.com>
2025-10-12 10:47:48 -0400

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,24 +1,37 @@
-// Firebase initialization shared by all pages
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-    import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-    import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-    import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, collection, addDoc, doc, getDoc, setDoc, updateDoc, query, orderBy, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-    export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAyWD_KLFAVDOFW7FyHPHYGG1gge2go2AY",
   authDomain: "morrow-industries.firebaseapp.com",
-  databaseURL: "https://morrow-industries-default-rtdb.firebaseio.com",
   projectId: "morrow-industries",
   storageBucket: "morrow-industries.appspot.com",
   messagingSenderId: "75190213345",
   appId: "1:75190213345:web:a521b54a1c8e00b68d75a0"
 };
 
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const auth = getAuth(app);
-    const storage = getStorage(app);
-    const provider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-    // Export to importers
-    export { app, db, auth, storage, provider, signInWithPopup, onAuthStateChanged, signOut };
+export {
+  app,
+  db,
+  auth,
+  provider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+};
