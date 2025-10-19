2bb89d73490e07227bf26d01f6a1e768671cd888
Randal Morrow <58852805+rmorrow23@users.noreply.github.com>
2025-10-18 23:31:45 -0500

    Update maintenance-listener.js

--- assets/js/maintenance-listener.js
+++ assets/js/maintenance-listener.js
@@ -3,7 +3,7 @@ import { db } from "/assets/js/firebase-init.js";
 import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const maintenanceRef = doc(db, "site", "config");
-const maintenancePage = "/maintenance.html"; // path to your maintenance page
+const maintenancePage = "/maintenance"; // path to your maintenance page
 
 onSnapshot(maintenanceRef, (snap) => {
   if (snap.exists() && snap.data().maintenanceMode === true) {
